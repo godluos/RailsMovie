78f72379e024c0c9e46e37aa60eecadef1b06eb3
godluos <godluos@gmail.com>
2017-03-22 22:03:46 +0800

    add all the crud

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,42 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+      redirect_to movies_path
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+      redirect_to movies_path, notice: "更新成功"
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:alert] = "已删除电影"
+    redirect_to movies_path
+  end
+
+private
+
+   def movie_params
+   params.require(:movie).permit(:title, :description)
+   end
 end
